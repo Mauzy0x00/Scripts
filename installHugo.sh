0ff9bace711c2e39c35c847c923a33d7d98cf449
Mauzy <127632417+Mauzy0x00@users.noreply.github.com>
2024-09-17 23:14:54 -0400

    Run more testing fix a bunch of stuff

--- installHugo.sh
+++ installHugo.sh
@@ -5,66 +5,81 @@
 #   https://github.com/gohugoio/hugo?tab=readme-ov-file
 #   https://go.dev/doc/install
 #   https://sass-lang.com/install/
+#   https://unix.stackexchange.com/questions/26047/how-to-correctly-add-a-path-to-path
 #
-# Notes: Edit the download links for the release versions you wish to use
+# Notes: Edit the download links for the release versions you wish to use. 
 
 # Update && upgrade system
 printf "\nUpdating and upgrading system...\n"
 sudo apt update && sudo apt upgrade -y
 
+# Install GCC & G++
+sudo apt install gcc g++
+
 # Install Go
+printf "\n\n"
 read -p "Is Go 1.23.1 the correct version to install? (y/n) " go_version_confirm
 if [[ "$go_version_confirm" == "y" ]]; then
-    printf "\nDownloading Go 1.23.1...\n"
+    printf "\n\nDownloading Go 1.23.1...\n"
     wget https://go.dev/dl/go1.23.1.linux-amd64.tar.gz
 
     printf "\nRemoving any existing Go installation and installing Go 1.23.1...\n"
     sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.1.linux-amd64.tar.gz
 
     printf "\nAdding Go to PATH...\n"
-    export PATH=$PATH:/usr/local/go/bin
+    echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.profile
 
-    printf "\nVerifying Go installation...\n"
+    printf "\nSetting GOPATH to ~/.go ...\n"
+    echo 'GOPATH=~/.go' >> ~/.profile
+    source ~/.profile
+
+    printf "\n\nVerifying Go installation...\n"
+    source ~/.profile
     go version
 else
-    printf "\nSkipping Go installation.\n"
+    printf "\n\nSkipping Go installation.\n"
 fi
 
 # Install Dart Sass
+printf "\n\n"
 read -p "Is Dart Sass 1.78.0 the correct version to install? (y/n) " sass_version_confirm
 if [[ "$sass_version_confirm" == "y" ]]; then
-    printf "\nDownloading Dart Sass 1.78.0...\n"
+    printf "\n\nDownloading Dart Sass 1.78.0...\n"
     wget https://github.com/sass/dart-sass/releases/download/1.78.0/dart-sass-1.78.0-linux-x64.tar.gz
 
     printf "\nInstalling Dart Sass...\n"
     sudo tar -C /usr/local -xzf dart-sass-1.78.0-linux-x64.tar.gz
 
     printf "\nAdding Dart Sass to PATH...\n"
-    export PATH=$PATH:/usr/local/dart-sass
+    echo 'PATH=$PATH:/usr/local/dart-sass' >> ~/.profile
 
-    printf "\nVerifying Dart Sass installation...\n"
+    printf "\n\nVerifying Dart Sass installation...\n"
+    source ~/.profile
     sass --version
 else
-    printf "\nSkipping Dart Sass installation.\n"
+    printf "\n\nSkipping Dart Sass installation.\n"
 fi
 
 # Clean up
-printf "\nCleaning up downloaded files...\n"
+printf "\n\nCleaning up downloaded files...\n"
 rm go1.23.1.linux-amd64.tar.gz dart-sass-1.78.0-linux-x64.tar.gz
 
 # Install Hugo
+printf "\n\n"
 read -p "Do you want to build and install Hugo with the extended edition? (y/n) " hugo_confirm
 if [[ "$hugo_confirm" == "y" ]]; then
-    printf "\nBuilding and installing Hugo extended edition...\n"
+    printf "\n\nBuilding and installing Hugo extended edition...\n"
     CGO_ENABLED=1 go install -tags extended github.com/gohugoio/hugo@latest
 
     printf "\nAdding Hugo to PATH...\n"
-    export PATH=$PATH:~/go/bin
+    echo 'PATH=$PATH:~/.go/bin' >> ~/.profile
+    source ~/.profile
 
     printf "\nVerifying Hugo installation...\n"
     hugo version
 else
-    printf "\nSkipping Hugo installation.\n"
+    printf "\n\nSkipping Hugo installation.\n"
 fi
 
-printf "\nInstallation complete!\n"  
+printf "\n\nInstallation complete!\n"  
+printf "If your commands aren't appearing, try running 'source ./profile' again!"
